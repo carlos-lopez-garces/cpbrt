cf2c23dfb0adfc95bea593e0125aa87e5e3f5f1a
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2020-10-20 11:09:06 -0700

    Add Infinity

--- src/core/pbrt.h
+++ src/core/pbrt.h
@@ -7,6 +7,8 @@
 #include <string>
 #include <vector>
 
+// Global forward declarations.
+
 // Choose between float and double for most floating-point declarations
 // at compile time.
 #ifdef CPBRT_FLOAT_AS_DOUBLE
@@ -14,3 +16,12 @@ typedef double Float;
 #else
 typedef float Float;
 #endif // CPBRT_FLOAT_AS_DOUBLE
+
+// Global constants.
+
+// Only so that you don't have to type std::numeric_limits<Float> ... 
+#ifdef _MSC_VER
+#define Infinity std::numeric_limits<Float>::infinity()
+#else
+static CPBRT_CONSTEXPR Float Infinity = std::numeric_limits<Float>::infinity();
+#endif // _MSC_VER
