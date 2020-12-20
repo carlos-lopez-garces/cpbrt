0192f7f8cd100f407c1b8b466ec34233eec79e11
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2020-12-19 21:50:09 -0800

    Add GlobalSampler

--- src/core/sampler.cpp
+++ src/core/sampler.cpp
@@ -97,3 +97,88 @@ Float PixelSampler::Get2D() {
         return rng.UniformFloat();
     }
 }
+
+void GlobalSampler::StartPixel(const Point2i &p) {
+    Sampler::StartPixel(p);
+    
+    dimension = 0;
+    intervalSampleIndex = GetIndexForSample(0);
+
+    // Compute arrayEndDim for dimensions used for array samples.
+    arrayEndDim = arrayStartDim + sampleArray1D.size() + 2*sampleArray2D.size();
+    
+    // Compute 1D array samples for GlobalSampler.
+    for (size_t i = 0; i < samples1DArraySizes.size(); ++i) {
+        // samples1DArraySizes records the number of dimensions per sample of a given
+        // array request.
+        int nSamples = samples1DArraySizes[i] * samplesPerPixel;
+
+        if (int j = 0; j < nSamples; ++j) {
+            int64_t index = GetIndexForSample(j);
+            sampleArray1D[i][j] = SampleDimension(index, arrayStartDim + i);
+        }
+    }
+
+    // Compute 2D array samples for GlobalSampler.
+    for (size_t i = 0; i < samples2DArraySizes.size(); ++i) {
+        // samples2DArraySizes records the number of dimensions per sample of a given
+        // array request.
+        int nSamples = samples2DArraySizes[i] * samplesPerPixel;
+
+        if (int j = 0; j < nSamples; ++j) {
+            int64_t index = GetIndexForSample(j);
+            sampleArray2D[i][j] = SampleDimension(index, arrayStartDim + i);
+        }
+    }
+}
+
+bool GlobalSampler::StartNextSample() {
+    // There's a distinction to make between samples and pixel samples: a sample is the value
+    // generated for a given dimension (or 2 at once, if the sample is 2D); a pixel sample is
+    // the collection of values generated for all of its dimensions.
+    //
+    // StartNextSample() starts a new pixel sample. That's why the recorded next dimension is
+    // reset.
+    dimension = 0;
+
+    intervalSampleIndex = GetIndexForSample(currentPixelSampleIndex + 1);
+
+    return Sampler::StartNextSample();
+}
+
+bool GlobalSampler::SetSampleNumber(int64_t sampleNum) {
+    // There's a distinction to make between samples and pixel samples: a sample is the value
+    // generated for a given dimension (or 2 at once, if the sample is 2D); a pixel sample is
+    // the collection of values generated for all of its dimensions.
+    //
+    // SetSampleNumber() sets the current pixel sample. That's why the recorded next dimension
+    // is reset.
+    dimension = 0;
+
+    intervalSampleIndex = GetIndexForSample(sampleNum);
+
+    return Sampler::SetSampleNumber(sampleNum);
+}
+
+Float GlobalSampler::Get1D() {
+    if (dimension >= arrayStartDim && dimension < arrayEndDim) {
+        dimension = arrayEndDim;
+    }
+
+    return SampleDimension(intervalSampleIndex, dimension++);
+}
+
+Point2f GlobalSampler::Get2D() {
+    if (dimension + 1 >= arrayStartDim && dimension < arrayEndDim) {
+        dimension = arrayEndDim;
+    }
+
+    Point2f p(
+        SampleDimension(intervalSampleIndex, dimension),
+        SampleDimension(intervalSampleIndex, dimension+1)
+    );
+
+    dimension += 2;
+
+    return p;
+}
--- src/core/sampler.h
+++ src/core/sampler.h
@@ -13,10 +13,10 @@
 // [0,1) floating-point number, without exception.
 class Sampler {
 private:
-    // Offset into sampleArray1D of the start of the array of values of the current sample. 
+    // Offset into sampleArray1D of the start of the array of samples of the current dimension. 
     size_t array1DOffset;
 
-    // Offset into sampleArray2D of the start of the array of values of the current sample.
+    // Offset into sampleArray2D of the start of the array of samples of the current dimension.
     size_t array2DOffset;
 
 protected:
@@ -127,4 +127,51 @@ public:
             samples2D.push_back(std::vector<Point2f>(samplesPerPixel));
         }
     }
+
+    // Overriden.
+    bool StartNextSample();
+    bool SetSampleNumber(int64_t sampleIndex);
+    Float Get1D();
+    Point2f Get2D();
+};
+
+// GlobalSampler generates consecutive samples that are spread across the entire image, for
+// completely different pixels in succession.
+class GlobalSampler : public Sampler {
+private:
+    // Next dimension to generate.
+    int dimension;
+
+    // ?
+    int64_t intervalSampleIndex;
+
+    // An individual 1D or 2D sample may be requested, or whole arrays of them. Some samplers
+    // generate samples of lesser and lesser quality; for example, the sample for the 1st
+    // dimension could be of higher quality than that of the 6th.
+    //
+    // Now, samples for CameraSamples are very important. Thus, the first 5 dimensions are
+    // the ones that a Camera uses because they are expected to be high-quality.
+    //
+    // These dimensions for the Camera are requested by the unit, so the first 5 dimensions
+    // are requested in 1s or 2s (via Get1D() and Get2D()). Requests by array are made only
+    // over the subsequent dimensions; dimensions [arrayStartDim, arrayEndDim], specifically.
+    // Dimensions after arrayEndDim are again for unit requests.
+    static const int arrayStartDim = 5;
+    int arrayEndDim;
+
+public:
+    GlobalSampler(int64_t samplesPerPixel) : Sampler(samplesPerPixel) {}
+
+    // These 2 functions adapt the base Sampler interface to the needs of a GlobalSampler.
+    // A GlobalSampler indexes its samples differently and these functions map between the 2
+    // indexing schemes.
+    virtual int64_t GetIndexForSample(int64_t sampleNum) const = 0;
+    virtual Float SampleDimension(int64_t index, int dimension) const = 0;
+
+    // Overriden.
+    void StartPixel(const Point2i &p);
+    bool StartNextSample();
+    bool SetSampleNumber(int64_t sampleIndex);
+    Float Get1D();
+    Point2f Get2D();
 };
