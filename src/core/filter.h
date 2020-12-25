1129a6298fbd1c7cdbb1e9f38765bf6202e973d9
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2020-12-24 16:35:45 -0800

    Create src/core/filter and add Filter

---
+++ src/core/filter.h
@@ -0,0 +1,16 @@
+#include "geometry.h"
+
+class Filter {
+public:
+    const Vector2f radius;
+    const Vector2f invRadius;
+
+    // 2*radius is the filter's extent, i.e. the interval that it spans before becoming
+    // or falling off to 0.
+    Filter(const Vector2f &radius) 
+        : radius(radius), invRadius(Vector2f(1 / radius.x, 1 / radius.y)) {}
+
+    // Computes the weight for a sample point (sampleX, sampleY) expressed in
+    // relation to the filter's origin (x, y), that is, (x - sampleX, y - sampleY).
+    virtual Float Evaluate(const Point2f &p) const = 0;
+};
