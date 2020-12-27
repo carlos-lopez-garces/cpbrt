15227d60a250c68bba15902a1422f47773396bbe
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2020-12-26 23:37:50 -0800

    Add FilmTile

--- src/core/film.cpp
+++ src/core/film.cpp
@@ -21,3 +21,74 @@ Bounds2f Film::GetPhysicalExtent() const {
 
     return Bounds2f(Point2f(-x/2, -y/2), Point2f(x/2, y/2));
 }
+
+// sampleBounds is the discrete pixel area that the film tile will cover. By handing out
+// a std::unique_ptr to the FilmTile to the caller, the function is transferring ownership of
+// the tile to the caller.
+std::unique_ptr<FilmTile> Film::GetFilmTile(const Bounds2i &sampleBounds) {
+    // Bound image pixels that samples in sampleBounds contribute to.
+    
+    // Discrete-to-continuous pixel coordinate mapping is: c = d - 0.5.
+    // Conversion to continuous coordinates is necessary because the filter's radius is a
+    // floating-point number.
+    Vector2f halfPixel = Vector2f(0.5f, 0.5f);
+    Bounds2f floatBounds = (Bounds2f) sampleBounds;
+    // Upper-left corner of discrete pixel area expanded by filter's radius. (The y-axis increases downwards.)
+    Point2i p0 = (Point2i) Ceil(floatBounds.pMin - halfPixel - filter->radius);
+    // Lower-right corner.
+    Point2i p1 = (Point2i) Floor(floatBounds.pMax - halfPixel + filter->radius) + Point2i(1, 1);
+
+    // Shave off the pixels that are outside the image.
+    Bounds2i tilePixelBounds = Intersect(Bounds2i(p0, p1), croppedPixelBounds);
+
+    return std::unique_ptr<FilmTile>(
+        new FilmTile(tilePixelBounds, filter->radius, filterTable, filterTableWidth)
+    );
+}
+
+// By receiving the FilmTile in a std::unique_ptr, the function is receiving ownership of the
+// tile from the caller. Upon return, when the std::unique_ptr goes out of scope, the tile gets
+// freed.
+void Film::MergeFilmTile(std::unique_ptr<FilmTile> tile) {
+    std::lock_guard<std::mutex> lock(mutex);
+    for (Point2i pixel : tile->GetPixelBounds()) {
+        // Merge pixel into Film::pixels.
+
+        const FilmTilePixel &tilePixel = tile->GetPixel(pixel);
+        Pixel &mergePixel = GetPixel(pixel);
+
+        Float xyz[3];
+        tilePixel.contribSum.ToXYZ(xyz);
+        for (int i = 0; i < 3; ++i) {
+            mergePixel.xyz[i] += xyz[i];
+        }
+
+        mergePixel.filterWeightSum += tilePixel.filterWeightSum;
+    }
+}
+
+// ?
+void Film::SetImage(const Spectrum *image) const {
+    int nPixels = croppedPixelBounds.Area();
+    for (int i = 0; i < nPixels; ++i) {
+        Pixel &p = pixels[i];
+        image[i].ToXYZ(p.xyz);
+        p.filterWeightSum = 1;
+        p.splatXYZ[0] = p.splatXYZ[1] = p.splatXYZ[2] = 0;
+    }
+}
+
+// ?
+void Film::AddSplat(const Point2f &p, const Spectrum &v) {
+    if (!InsideExclusive((Point2i)p, croppedPixelBounds)) {
+        return;
+    }
+    
+    Float xyz[3];
+    v.ToXYZ(xyz);
+    
+    Pixel &pixel = GetPixel((Point2i)p);
+    for (int i = 0; i < 3; ++i) {
+        pixel.splatXYZ[i].Add(xyz[i]);
+    }
+}
--- src/core/film.h
+++ src/core/film.h
@@ -1,9 +1,11 @@
 #include <memory>
+#include <mutex>
 #include <string>
 
 #include "filter.h"
 #include "geometry.h"
 #include "parallel.h"
+#include "spectrum.h"
 
 class Film {
 private:
@@ -18,15 +20,20 @@ private:
 
         // To avoid cache line straddling.
         Float pad;
+
+        Pixel() { xyz[0] = xyz[1] = xyz[2] = filterWeightSum = 0; }
     };
 
     // All the pixels of the image, possibly cropped.
+    // Layout: [(x0, y0), ..., (xn-1, y0), (x0, y1), ..., (xn-1, y1), ...]
     std::unique_ptr<Pixel[]> pixels;
 
     // Stores filter weights computed at construction-time for 16x16 uniformly distributed
     // sample points over the 2D extent of the filter.
     static constexpr int filterTableWidth = 16;
     Float filterTable[filterTableWidth * filterTableWidth];
+
+    std::mutex mutex;
 
 public:
     // In pixels.
@@ -102,6 +109,12 @@ public:
         }
     }
 
+    Pixel &GetPixel(const Point2i &p) {
+        int width = croppedPixelBounds.pMax.x - croppedPixelBounds.pMin.x;
+        int offset = (p.x - croppedPixelBounds.pMin.x) + (p.y - croppedPixelBounds.pMin.y) * width;
+        return pixels[offset];
+    }
+
     // Computes the bounds of the rectangular sampling region for the (possibly cropped) image.
     // These bounds extend beyond the film or crop window area to accomodate the entire extent
     // of the filters centered at the boundary pixels or the pixels near them. 
@@ -109,4 +122,122 @@ public:
 
     // Computes the physical bounds of the film.
     Bounds2f GetPhysicalExtent() const;
+
+    // The film is divided into nonoverlapping tiles that rendering threads can sample and filter
+    // independently.
+    std::unique_ptr<FilmTile> GetFilmTile(const Bounds2i &sampleBounds);
+
+    void MergeFilmTile(std::unique_ptr<FilmTile> tile);
+
+    // ?
+    void SetImage(const Spectrum *image) const;
+
+    // ?
+    void AddSplat(const Point2f &p, const Spectrum &v);
+};
+
+class FilmTile {
+private:
+    // Discrete pixel area of the film that the film tile covers.
+    const Bounds2i pixelBounds;
+    
+    // Pixels enclosed by the film tile.
+    // Layout: [(x0, y0), ..., (xn-1, y0), (x0, y1), ..., (xn-1, y1), ...]
+    std::vector<FilmTilePixel> pixels;
+
+    const Vector2f filterRadius;
+    const Vector2f invFilterRadius;
+    const Float *filterTable;
+
+    // Filter table width (either number of rows or columns of square table).
+    const int filterTableSize;
+
+public:
+    FilmTile(
+        const Bounds2i &pixelBounds,
+        const Vector2f &filterRadius,
+        const Float *filterTable,
+        int filterTableSize
+    ) : pixelBounds(pixelBounds),
+        filterRadius(filterRadius),
+        invFilterRadius(1 / filterRadius.x, 1 / filterRadius.y),
+        filterTable(filterTable),
+        filterTableSize(filterTableSize) {
+        
+        pixels = std::vector<FilmTilePixel>(std::max(0, pixelBounds.Area()));
+    }
+
+    FilmTilePixel &GetPixel(const Point2i &p) {
+        int width = pixelBounds.pMax.x - pixelBounds.pMin.x;
+        int offset = (p.x - pixelBounds.pMin.x) + (p.y - pixelBounds.pMin.y) * width;
+        return pixels[offset];
+    }
+
+    void AddSample(const Point2f &pFilm, const Spectrum &L, Float sampleWeight = 1.f) {
+        // Compute discrete bounds that enclose pixels that the sample contributes to
+        // according to the filter's extent. The filter is placed at the center of the
+        // discrete pixel, not at the continuous pixel coordinate of the sample.
+
+        // Continuous-to-discrete pixel coordinate mapping is: d = c - 0.5.
+        Point2f pFilmDiscrete = pFilm - Vector2f(0.5f, 0.5f);
+        Point2i p0 = (Point2i) Ceil(pFilmDiscrete - filterRadius);
+        Point2i p1 = (Point2i) Floor(pFilmDiscrete + filterRadius) + Point2i(1, 1);
+        // Shave off the pixels that are outside the image.
+        p0 = Max(p0, pixelBounds.pMin);
+        p1 = Min(p1, pixelBounds.pMax);
+
+        // Precompute x and y filter table offsets (array indices). The offset is y*filterTableSize + x,
+        // that's how the table was built by the Film constructor: for (y...width) { for (x...width) },
+        // where width = filterTableSize.
+        int *ifx = ALLOCA(int, p1.x - p0.x);
+        for (int x = p0.x; x < p1.x; ++x) {
+            // x is the x coordinate of a discrete pixel that is within the reach of the filter
+            // centered at the discrete pixel pFilmDiscrete that contains the input sample point
+            // pFilm. The distance between the 2 determines the wanted filter weight table x offset.
+            Float fx = std::abs((x - pFilmDiscrete.x) * invFilterRadius.x * filterTableSize);
+            ifx[x - p0.x] = std::min((int) std::floor(fx), filterTableSize - 1);
+        }
+        int *ify = ALLOCA(int, p1.y - p0.y);
+        for (int y = p0.y; y < p1.y; ++y) {
+            Float fy = std::abs((y - pFilmDiscrete.y) * invFilterRadius.y * filterTableSize);
+            ify[y - p0.y] = std::min((int) std::floor(fy), filterTableSize - 1);
+        }
+
+        // Loop over discrete pixel coordinates within filter extent and add sample to
+        // pixel arrays.
+        for (int y = p0.y; y < p1.y; ++y) {
+            for (int x = p0.x; x < p1.x; ++x) {
+                // Evaluate filter weight for sample coordinate (look it up, really).
+                //
+                // This is how the filter would be evaluated if we weren't precomputing them:
+                // filterWeight = filter->Evaluate(Point2i(x - pFilmDiscrete.x, y - pFilmDiscrete.y));
+                int offset = ify[y - p0.y] * filterTableSize + ifx[x - p0.x];
+                Float filterWeight = filterTable[offset];
+
+                // Update pixel values with filtered sample contribution.
+                FilmTilePixel &pixel = GetPixel(Point2i(x, y));
+                pixel.contribSum += L * sampleWeight * filterWeight;
+                pixel.filterWeightSum += filterWeight;
+            }
+        }
+    }
+
+    Bounds2i GetPixelBounds() const {
+        return pixelBounds;
+    }
+};
+
+// Stores 2 running sums, the numerator and denominator of the quotient that defines the
+// pixel filtering/reconstruction equation:
+//
+// SUM_i(f(x-x_i, y-y_i)w(x_i, y_i)L(x_i, y_i))
+// --------------------------------------------
+// SUM_i(f(x-x_i, y-y_i)
+//
+// where f is the filter, w is the weight that the camera assigns to the sample, and L is
+// radiance value of the sample.
+struct FilmTilePixel {
+    // Converting constructor of Spectrum allows for initialization from float.
+    Spectrum contribSum = 0.f;
+    Float filterWeightSum = 0.f;
 };
