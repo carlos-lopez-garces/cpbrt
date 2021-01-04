8bf7dbe97b06604c354a26601f4ff47f2e4f069c
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-01-03 21:05:01 -0800

    Add BxDF

--- src/core/reflection.h
+++ src/core/reflection.h
@@ -1,5 +1,6 @@
 #include "cpbrt.h"
 #include "geometry.h"
+#include "spectrum.h"
 
 // The shading coordinate system is defined by the orthonormal basis {s, t, n} = {x, y, z},
 // where s and t are 2 orthogonal vectors tangent to the shaded point and n is the normal
@@ -81,3 +82,58 @@ inline Float CosDPhi(const Vector3f &wa, const Vector3f &wb) {
         -1, 1
     );
 }
+
+enum BxDFType {
+    BSDF_REFLECTION   = 1 << 0,
+    BSDF_TRANSMISSION = 1 << 1,
+    BSDF_DIFFUSE      = 1 << 2,
+    BSDF_GLOSSY       = 1 << 3,
+    BSDF_SPECULAR     = 1 << 4,
+    BSDF_ALL          = BSDF_REFLECTION | BSDF_TRANSMISSION | BSDF_DIFFUSE | BSDF_GLOSSY | BSDF_SPECULAR
+};
+
+// Interface for BRDF and BTDF.
+class BxDF {
+public:
+    const BxDFType type;
+
+    BxDF(BxDFType type) : type(type) {}
+
+    bool MatchesFlags(BxDFType t) const {
+        return (type & t) == type;
+    }
+
+    // Computes the spectral distribution of a radiometric quantity over wavelength in the
+    // given outgoing direction that corresponds to the given incident direction.
+    virtual Spectrum f(const Vector3f &wo, const Vector3f &wi) const = 0;
+
+    // Computes the spectral distribution of a radiometric quantity over wavelength in the
+    // given outgoing direction, computing also the (possibly unique) corresponding incident
+    // direction.
+    virtual Spectrum Sample_f(
+        const Vector3f &wo,
+        Vector3f *wi,
+        const Point2f &sample,
+        Float *pdf,
+        BxDFType *sampledType = nullptr
+    ) const;
+
+    // Computes the hemispherical-directional reflectance of the surface in the given outgoing
+    // direction when the spectral distribution is constant across all the incident directions
+    // (over the hemisphere; note that there's no particular input incident direction wi).
+    //
+    // Reflectance is the spectral distribution that describes the reflective (scattering)
+    // behavior of the surface. Reflectance alone does not compute radiance, but it dictates
+    // the spectral distribution of it.
+    //
+    // nSamples and samples are only used when the implementation can't compute it exactly using
+    // a closed form and must approximate it instead.
+    virtual Spectrum rho(const Vector3f &wo, int nSamples, const Point2f *samples) const;
+
+    // Computes the hemispherical-hemispherical reflectance of the surface.
+    // TODO: explain.
+    //
+    // nSamples and samples are only used when the implementation can't compute it exactly using
+    // a closed form and must approximate it instead.
+    virtual Spectrum rho(int nSamples, const Point2f *samples1, const Point2f *samples2) const;
+};
