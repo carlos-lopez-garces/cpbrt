ff21f8a06870a392c69a5c7f904eb579d88c6322
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-01-11 17:54:03 -0800

    Create src/core/texture and add TextureMapping2D and UVMapping2D

---
+++ src/core/texture.cpp
@@ -0,0 +1,35 @@
+#include "texture.h"
+
+Point2f UVMapping2D::Map(const SurfaceInteraction &si, Vector2f *dstdx, Vector2f *dstdy) const {
+    // Let T be the mapping (u,v) -> (s,t) as defined by the returned value of this Map function.
+    // Its derivative is then:
+    //
+    // T'(u,v) = ((ds/du, ds/dv), (dt/du, dt/dv)) = ((su, 0), (0, sv))
+    //
+    // where su and sv are the scaling factors and are not to be confused with the s coordinate;
+    // likewise, du and dv in the returned value of this Map function are the shifting deltas and
+    // are not to be confused with the du and dv partial differentials used above.
+    //
+    // In order to match the texture function sampling rate to the image sampling rate, we need
+    // to make u and v functions of x and y pixel coordinates: u(x,y) and v(x,y).
+    //
+    // T thus becomes a vector-valued function of the vector (x,y): T: (u(x,y),v(x,y)) -> (s,t).
+    // Its derivative is then:
+    //
+    // T'(u(x,y), v(x,y)) = (dst/dx, dst/dy)
+    //
+    // where the dstdx and dstdy partial derivatives are given by the chain rule:
+    //
+    // dst/dx = (ds/dx, dt/dx) = (du/dx*ds/du + dv/dx*ds/dv, du/dx*dt/du + dv/dx*dt/dv)
+    //                         = (du/dx*su    + dv/dx*0,     du/dx*0     + dv/dx*sv)
+    //                         = (du/dx*su, dv/dx*sv)
+    //
+    // dst/dy = (ds/dy, dt/dy) = (du/dy*ds/du + dv/dy*ds/dv, du/dy*dt/du + dv/dy*dt/dv)
+    //                         = (du/dy*su    + dv/dy*0,     du/dy*0     + dv/dy*sv)
+    //                         = (du/dy*su, dv/dy*sv)
+    *dstdx = Vector2f(si.dudx * su, si.dvdx * sv);
+    *dstdy = Vector2f(si.dudy * su, si.dvdy * sv);
+
+    // Scale and shift the (u,v) coordinate.
+    return Point2f(su * si.uv[0] + du, sv * si.uv[1] + dv);
+}
---
+++ src/core/texture.h
@@ -0,0 +1,34 @@
+#include "interaction.h"
+#include "geometry.h"
+
+class TextureMapping2D {
+public:
+    // Maps the intersection point in the SurfaceInteraction to a 2D (s,t) texture
+    // coordinate. dst/dx and dst/dy are the partial derivatives of this mapping
+    // with respect to a pixel-coordinate change in raster space; they tell the
+    // texture function sampling rate that corresponds to the image function sampling
+    // rate carried by the SurfaceInteraction.
+    virtual Point2f Map(
+        const SurfaceInteraction &si,
+        Vector2f *dstdx,
+        Vector2f *dstdy
+    ) const = 0;
+};
+
+class UVMapping2D : public TextureMapping2D {
+private:
+    // Scaling factors for (u,v) coordinates applied when mapping a point on a surface to
+    // a texture (s,t)=(u,v) coordinate.
+    const Float su;
+    const Float sv;
+
+    // Shifting deltas for (u,v) coordinates applied when mapping a point on a surface to
+    // a texture (s,t)=(u,v) coordinate.
+    const Float du;
+    const Float dv;
+
+public:
+    UVMapping2D(Float su, Float sv, Float du, Float dv) : su(su), sv(sv), du(du), dv(dv) {}
+
+    Point2f Map(const SurfaceInteraction &si, Vector2f *dstdx, Vector2f *dstdy) const; 
+};
