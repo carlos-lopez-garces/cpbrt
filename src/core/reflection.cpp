49decc825d8f4986e4297f5b951b794ae6fd3ca1
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-03-10 18:39:00 -0800

    Add BxDF::Sample_f and BxDF::Pdf

--- src/core/reflection.cpp
+++ src/core/reflection.cpp
@@ -1,4 +1,43 @@
+#include "cpbrt.h"
 #include "reflection.h"
+#include "sampling.h"
+
+Spectrum BxDF::Sample_f(
+    const Vector3f &wo,
+    Vector3f *wi,
+    const Point2f &u,
+    Float *pdf,
+    BxDFType *sampledType
+) const {
+    // The incident direction wi corresponding to the outgoing direction wo may be any
+    // one on the hemisphere centered at the point u and on the side of the surface where
+    // wo exits (which isn't always the side of the surface's normal). This incident direction
+    // wi is sampled from a cosine-weighted distribution, which assigns a higher probability
+    // to directions at the top of the hemisphere than those at the bottom (why is this
+    // desirable?).
+    //
+    // This base implementation of Sample_f is not for BTDFs, which should sample wi from the
+    // opposite hemisphere, nor is it for BRDFs with delta distribution (which map an outgoing
+    // direction wo to a single fixed incident direction wi).
+    *wi = CosineSampleHemisphere(u);
+
+    if (wo.z < 0) {
+        // CosineSampleHemisphere samples the hemisphere on the normal's side. When the outgoing
+        // direction wo lies on the hemisphere opposite to the normal, wi will have been sampled
+        // from the wrong hemisphere. Bring it to wo's side.
+        wi->z *= -1;
+    }
+
+    *pdf = Pdf(wo, *wi);
+    return f(wo, *wi);
+}
+
+// Computes the PDF with which the base implementation of Sample_f samples the incident direction
+// wi. This PDF is of a cosine-weighted distribution: p(w)=r*cos(theta)/pi, where r=1 is the radius
+// of the unit hemisphere and theta is measured from the hemisphere's axis.
+Float BxDF::Pdf(const Vector3f &wo, const Vector3f &wi) const {
+    return SameHemisphere(wo, wi) ? AbsCosTheta(wi) * InvPi : 0;
+}
 
 Spectrum ScaledBxDF::f(const Vector3f &wo, const Vector3f &wi) const {
     // The product of 2 spectrums is sample-wise.
