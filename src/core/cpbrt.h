471d61b1c2d62d65272b9d53b1f9c8959de86fa0
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-05-25 23:34:47 -0700

    Add Inv2Pi, Inv4Pi, and Sqrt2

--- src/core/cpbrt.h
+++ src/core/cpbrt.h
@@ -64,10 +64,13 @@ typedef RGBSpectrum Spectrum;
 static CPBRT_CONSTEXPR Float Infinity = std::numeric_limits<Float>::infinity();
 #endif // _MSC_VER
 
-static const Float Pi = 3.14159265358979323846;
-static const Float InvPi = 0.31830988618379067154;
+static const Float Pi      = 3.14159265358979323846;
+static const Float InvPi   = 0.31830988618379067154;
+static const Float Inv2Pi  = 0.15915494309189533577;
+static const Float Inv4Pi  = 0.07957747154594766788;
 static const Float PiOver2 = 1.57079632679489661923;
 static const Float PiOver4 = 0.78539816339744830961;
+static const Float Sqrt2   = 1.41421356237309504880;
 
 // Dynamic memory allocation on the stack.
 #ifdef _MSC_VER
