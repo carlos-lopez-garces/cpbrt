9145381741868d3495d60d15cb31ea298950e504
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2021-05-28 23:37:42 -0700

    Add MediumInterface constructors

--- src/core/medium.h
+++ src/core/medium.h
@@ -9,6 +9,13 @@ struct MediumInterface {
     // TODO: implement.
     const Medium *inside;
     const Medium *outside;
+
+    MediumInterface() : inside(nullptr), outside(nullptr) {}
+    
+    MediumInterface(const Medium *medium) : inside(medium), outside(medium) {}
+    
+    MediumInterface(const Medium *inside, const Medium *outside)
+        : inside(inside), outside(outside) {}
 };
 
 #endif // PBRT_CORE_MEDIUM_H
