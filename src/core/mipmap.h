02b9892c46f9807c2495f6f9fc6b9bea88af5186
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2022-03-25 15:57:17 -0700

    Add MIPMap::resampleWeights

--- src/core/mipmap.h
+++ src/core/mipmap.h
@@ -2,12 +2,25 @@
 #define CPBRT_CORE_MIPMAP_H
 
 #include "cpbrt.h"
+#include "texture.h"
+#include "parallel.h"
 
 // What to do when the supplied texture coordinates are outside [0,1].
 enum class ImageWrap {
     Repeat,
     Black,
     Clamp
+};
+
+// Return type of MIPMap::resampleWeights.
+struct ResampleWeight {
+    // Offset to the first of the 4 original texels that contribute to the value
+    // of the given texel in the resampled, higher-resolution image. These 4 texels
+    // are contiguous.
+    int firstTexel;
+    // 4 texels of the original image contribute to the value of 1 pixel in a
+    // resampled, higher-resolution image.
+    Float weight[4];
 };
 
 template <typename T> class MIPMap {
@@ -41,10 +54,47 @@ public:
             // then resample the result in the t direction to obtain the final image of
             // resolution (s^2, t^2). 
 
-            // Resample in the s direction.
+            // Resample (magnify) in the s direction. The resulting image will have a
+            // resolution of (resolutionPow2[0], resolution[1]).
+            std::unique_ptr<ResampleWeight[]> sWeights = resampleWeights(resolution.x, resolutionPow2.x);
+            // Allocate space for the final image of resolution (resolutionPow2[0], resolutionPow2[1]),
+            // not just for this pass in the s direction.
+            resampledImage.reset(new T[resolutionPow2.x * resolutionPow2.y]);
 
-            // Resample in the t direction.
+            // Resample (magnify) in the t direction.
         }
+    }
+
+private:
+    // resampleWeights returns one ResampleWeight object per original texel in column or row
+    // of the original image. The information returned, which is for one column or one row,
+    // applies to the rest of the columns or rows of the image.
+    std::unique_ptr<ResampleWeight[]> resampleWeights(int oldResolution, int newResolution) {
+        std::unique_ptr<ResampleWeight> weight(new ResampleWeight[newResolution]);
+        // 2 original texels. 4 texels end up contributing to each new texel in the resampled,
+        // higher resolution image: 2 in the s and 2 in the t direction.
+        Float filterWidth = 2.f;
+        for (int i = 0; i < newResolution; ++i) {
+            // Center of the filter in the original image. Since the filter's width is 2,
+            // the 4 texels that surround this center contribute to the value of the new
+            // texel in the resampled, higher resolution image.
+            Float center = (i+0.5f) * oldResolution / newResolution;
+            // Index of the 1st of the 4 original texels that contribute to this new texel.
+            weight[i].firstTexel = std::floor((center - filterWidth) + 0.5f);
+            for (int j = 0; j < 4; ++j) {
+                Float originalTexelPosition = weight[i].firstTexel + j + 0.5f;
+                weight[i].weight[j] = LanczosFilter((originalTexelPosition - center) / filterWidth);
+            }
+
+            // Normalize.
+            Float reciprocalSumWeights = 1 / (weight[i].weight[0] + weight[i].weight[1] + weight[i].weight[2] + weight[i].weight[3]);
+            for (int j = 0; j < 4; ++j) {
+                // Normalize the weight of each contributing texel, because the sum might not
+                // sum to 1 (depends on the filter).
+                weight[i].weight[j] *= reciprocalSumWeights;
+            }
+        }
+        return weight;
     }
 };
 
