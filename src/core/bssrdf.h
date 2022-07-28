cf47883a1479fb08afe9e8e705b3bae710256148
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2022-07-28 09:12:29 -0400

    Add BSSRDF::S

--- src/core/bssrdf.h
+++ src/core/bssrdf.h
@@ -12,6 +12,10 @@ public:
     // po is the SurfaceInteraction at the point where exitant radiance is to be computed
     // and eta is the index of refraction of the medium.
     BSSRDF(const SurfaceInteraction &po, Float eta) : po(po), eta(eta) {}
+
+    // Computes the ratio of differential exitant radiance at po in the direction 
+    // SurfaceInteraction:wo to the incident differential flux at pi from direction wi.
+    virtual Spectrum S(const SurfaceInteraction &pi, const Vector3f &wi) = 0;
 };
 
 #endif // CPBRT_CORE_BSSRDF_H
