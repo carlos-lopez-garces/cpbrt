12ee1810617b0427d3123eda924a3d51422dbdbd
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2023-04-25 17:44:14 -0700

    Add Quadratic

--- src/core/cpbrt.h
+++ src/core/cpbrt.h
@@ -447,6 +447,31 @@ inline Float Log2(Float x) {
     return std::log(x) * invLog2;
 }
 
+inline bool Quadratic(Float a, Float b, Float c, Float *t0, Float *t1) {
+    double discrim = (double)b * (double)b - 4 * (double)a * (double)c;
+    if (discrim < 0) {
+        return false;
+    }
+
+    double rootDiscrim = std::sqrt(discrim);
+
+    double q;
+    if (b < 0) {
+        q = -.5 * (b - rootDiscrim);
+    } else {
+        q = -.5 * (b + rootDiscrim);
+    }
+
+    *t0 = q / a;
+    *t1 = c / q;
+
+    if (*t0 > *t1) {
+        std::swap(*t0, *t1);
+    }
+
+    return true;
+}
+
 extern Options CpbrtOptions;
 
 #endif // CPBRT_CORE_PBRT_H
