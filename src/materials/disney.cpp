aa281908dd5507a7d35bbe96823488f6956c00e7
Carlos López Garcés <carlos.lopez.garces@gmail.com>
2023-07-27 16:06:37 -0700

    Add DisneyRetroReflection to DisneyMaterial BSDF

--- src/materials/disney.cpp
+++ src/materials/disney.cpp
@@ -1,5 +1,6 @@
 #include "disney.h"
 #include "core/interaction.h"
+#include "core/microfacet.h"
 #include "core/reflection.h"
 
 void DisneyMaterial::ComputeScatteringFunctions(
@@ -14,16 +15,29 @@ void DisneyMaterial::ComputeScatteringFunctions(
     Spectrum colour = color->Evaluate(*si).Clamp();
     Float metallicWeight = metallic->Evaluate(*si);
     Float diffuseWeight = 1.f - metallicWeight;
-    // Float IOR = eta->Evaluate(*si);
+    Float rough = roughness->Evaluate(*si);
 
-    si->bsdf->Add(ARENA_ALLOC(arena, DisneyDiffuseReflection)(
-        colour * diffuseWeight
-    ));
+    if (diffuseWeight) {
+        // DisneyDiffuseReflection implements the diffuse term of the f_d equation (4) in
+        // Extending the Disney BRDF to a BSDF with Integrated Subsurface Scattering by Burley.
+        // The f_retro-reflection term of f_d is implemented in DisneyRetroReflection.
+        si->bsdf->Add(ARENA_ALLOC(arena, DisneyDiffuseReflection)(
+            // The diffuse response decreases as the metallic parameter increases.
+            colour * diffuseWeight
+        ));
+
+        // DisneyRetroReflection implements the f_retro-reflection term of the f_d equation (4) in
+        // Extending the Disney BRDF to a BSDF with Integrated Subsurface Scattering by Burley.
+        si->bsdf->Add(ARENA_ALLOC(arena, DisneyRetroReflection)(
+            colour * diffuseWeight,
+            rough
+        ));
+    }
 }
 
 DisneyMaterial *CreateDisneyMaterial(const TextureParams &mp) {
     std::shared_ptr<Texture<Spectrum>> color = mp.GetSpectrumTexture("color", Spectrum(0.5f));
     std::shared_ptr<Texture<Float>> metallic = mp.GetFloatTexture("metallic", 0.f);
-    // std::shared_ptr<Texture<Float>> eta = mp.GetFloatTexture("eta", 1.5f);
-    return new DisneyMaterial(color, metallic);
+    std::shared_ptr<Texture<Float>> roughness = mp.GetFloatTexture("roughness", 0.5f);
+    return new DisneyMaterial(color, metallic, roughness);
 }
--- src/materials/disney.h
+++ src/materials/disney.h
@@ -10,15 +10,20 @@ private:
     std::shared_ptr<Texture<Spectrum>> color;
     // In [0, 1], where 0 = dielectric and 1 = metallic. (Most parameters are in [0,1].)
     std::shared_ptr<Texture<Float>> metallic;
+    // Degree of roughness, in [0, 1], where 0 is smooth. Influences both the diffuse
+    // and specular responses.
+    std::shared_ptr<Texture<Float>> roughness;
     // Index of refraction IOR of the inside of the object.
     std::shared_ptr<Texture<Float>> eta;
 
 public:
     DisneyMaterial(
         const std::shared_ptr<Texture<Spectrum>> &color,
-        const std::shared_ptr<Texture<Float>> &metallic
+        const std::shared_ptr<Texture<Float>> &metallic,
+        const std::shared_ptr<Texture<Float>> &roughness
     ) : color(color),
-        metallic(metallic)
+        metallic(metallic),
+        roughness(roughness)
     {}
 
     // Evaluates BSDFs at intersection point. PathIntegrator and VolPathIntegrator
